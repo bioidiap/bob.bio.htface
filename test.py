5c366d15d9e3beffe2f047abc97a29eb4a909222
Tiago Pereira <tiago.pereira@partner.samsung.com>
2017-07-11 07:36:31 -0700

    Added a third inception layer

--- test.py
+++ test.py
@@ -20,84 +20,6 @@ l_rate = 0.01
 iterations = 100000
 
 
-def my_inception(inputs, scope='myInception', reuse=False, device="/cpu:0"):
-    slim = tf.contrib.slim
-
-    with variable_scope.variable_scope(scope, 'InceptionVx', [inputs], reuse=reuse):
-
-
-        #initializer = tf.contrib.layers.xavier_initializer(uniform=False, dtype=tf.float32, seed=10)
-
-        end_point = "conv1"
-        graph = layers.conv2d(inputs, 64, [5, 5], stride=2, scope=end_point)
-
-        end_point = "pool1"
-        graph = layers_lib.max_pool2d(graph, [2, 2], stride=1, scope=end_point)
-        #graph = slim.max_pool2d(graph, [2, 2], scope='pool1')
-
-        #graph = slim.conv2d(graph, 10, [3, 3], activation_fn=tf.nn.relu,
-        #                    stride=1,
-        #                    weights_initializer=initializer,
-        #                    scope='conv2', reuse=reuse)
-        #graph = slim.max_pool2d(graph, [2, 2], scope='pool2')
-
-        end_point = "conv2"
-        graph = layers.conv2d(graph, 64, [5, 5], stride=2, scope=end_point, normalizer_fn=layers_lib.batch_norm)
-
-        end_point = "pool2"
-        graph = layers_lib.max_pool2d(graph, [2, 2], stride=1, scope=end_point)
-
-        end_point = "conv3"
-        graph = layers.conv2d(graph, 32, [5, 5], stride=2, scope=end_point, normalizer_fn=layers_lib.batch_norm)
-
-        end_point = "pool3"
-        graph = layers_lib.max_pool2d(graph, [2, 2], stride=2, scope=end_point)
-
-
-        with variable_scope.variable_scope("Mixed2"):
-            with variable_scope.variable_scope('Branch_0'):
-                branch_0 = layers.conv2d(graph, 192, [1, 1], scope='Conv2d_0a_1x1', normalizer_fn=layers_lib.batch_norm)
-
-            with variable_scope.variable_scope('Branch_1'):
-                branch_1 = layers.conv2d(graph, 96, [1, 1], scope='Conv2d_0a_1x1', normalizer_fn=layers_lib.batch_norm)
-                branch_1 = layers.conv2d(
-                    branch_1, 208, [3, 3], scope='Conv2d_0b_3x3')
-
-            with variable_scope.variable_scope('Branch_2'):
-                branch_2 = layers.conv2d(graph, 16, [1, 1], scope='Conv2d_0a_1x1', normalizer_fn=layers_lib.batch_norm)
-                branch_2 = layers.conv2d(
-                    branch_2, 48, [3, 3], scope='Conv2d_0b_3x3', normalizer_fn=layers_lib.batch_norm)
-
-            #with variable_scope.variable_scope('Branch_3'):
-            #    branch_3 = layers_lib.max_pool2d(
-            #        graph, [3, 3], scope='MaxPool_0a_3x3')
-            #    branch_3 = layers.conv2d(
-            #        branch_3, 64, [1, 1], scope='Conv2d_0b_1x1')
-
-            #graph = array_ops.concat([branch_0, branch_1, branch_2, branch_3], 3)
-            graph = array_ops.concat([branch_0, branch_1, branch_2], 3)
-
-        with variable_scope.variable_scope('Logits'):
-            graph = layers_lib.avg_pool2d( graph, [7, 7], stride=1, scope='MaxPool_0a_7x7')
-
-            graph = layers_lib.dropout(graph, 0.4, scope='Dropout_0b')
-            graph = layers.conv2d(
-                graph,
-                75, [1, 1],
-                activation_fn=None,
-                normalizer_fn=None,
-                scope='Conv2d_0c_1x1')
-
-            #if spatial_squeeze:
-            #    logits = array_ops.squeeze(logits, [1, 2], name='SpatialSqueeze')
-        #import ipdb; ipdb.set_trace()
-        graph = slim.flatten(graph, scope='flatten1')
-        graph = layers_lib.softmax(graph)
-
-
-    return graph
-
-
 def my_inception_2(inputs, scope='myInception', reuse=False, device="/cpu:0"):
     slim = tf.contrib.slim
     initializer = tf.contrib.layers.xavier_initializer(seed=10)  # Weights initializer
@@ -187,6 +109,37 @@ def my_inception_2(inputs, scope='myInception', reuse=False, device="/cpu:0"):
                                            scope='Branch_3_5x5')
 
                 graph = array_ops.concat([branch_0, branch_1, branch_2, branch_3], 3)
+
+
+            with variable_scope.variable_scope("Inception_3"):
+                with tf.variable_scope('Branch_0'):
+                    branch_0 = slim.conv2d(graph, 32, [1, 1], stride=1, normalizer_fn=slim.batch_norm,
+                                           activation_fn=tf.nn.relu, scope='Branch_0_1x1')
+
+                with tf.variable_scope('Branch_1'):
+                    branch_1 = slim.conv2d(graph, 32, [1, 1], stride=1, activation_fn=tf.nn.relu,
+                                           normalizer_fn=slim.batch_norm,
+                                           scope='Branch_1_1x1')
+                    branch_1 = slim.conv2d(branch_1, 64, [3, 3], stride=1, activation_fn=tf.nn.relu,
+                                           normalizer_fn=slim.batch_norm,
+                                           scope='Branch_1_3x3')
+
+                with tf.variable_scope('Branch_2'):
+                    branch_2 = slim.conv2d(graph, 32, [1, 1], stride=1, activation_fn=tf.nn.relu,
+                                           normalizer_fn=slim.batch_norm,
+                                           scope='Branch_2_1x1')
+                    branch_2 = slim.conv2d(branch_2, 64, [5, 5], stride=1, activation_fn=tf.nn.relu,
+                                           normalizer_fn=slim.batch_norm,
+                                           scope='Branch_2_5x5')
+
+                with tf.variable_scope('Branch_3'):
+                    branch_3 = slim.max_pool2d(graph, [3, 3], padding="SAME", stride=1, scope='Branch_3_pool')
+                    branch_3 = slim.conv2d(branch_3, 32, [1, 1], stride=1, activation_fn=tf.nn.relu,
+                                           normalizer_fn=slim.batch_norm,
+                                           scope='Branch_3_5x5')
+
+                graph = array_ops.concat([branch_0, branch_1, branch_2, branch_3], 3)
+
 
             # 14 x 14 x 192 (32 + 64 + 64 + 32)
             graph = slim.flatten(graph, scope='flatten1')
