24d560cd7cc12079c3beee5beebfdb199c6f9a23
Tiago Pereira <tiago.pereira@partner.samsung.com>
2017-07-11 18:58:40 -0700

    CASIA Webface test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from bob.bio.htface.datashuffler import SiameseDiskHTFace, MeanOffsetHT
 from bob.learn.tensorflow.loss import ContrastiveLoss
 from bob.learn.tensorflow.trainers import SiameseTrainer, constant
 from bob.learn.tensorflow.network import Chopra
-from bob.learn.tensorflow.datashuffler import MeanOffset, ImageAugmentation
+from bob.learn.tensorflow.datashuffler import MeanOffset, ImageAugmentation, SiameseDisk
 import bob.io.base
 
 import tensorflow as tf
@@ -12,16 +12,37 @@ from tensorflow.contrib import layers
 from tensorflow.python.ops import array_ops
 from tensorflow.contrib.layers.python.layers import layers as layers_lib
 import os
+import numpy
 
 directory = "./temp/inception"
-#database_path = "/Users/tiago.pereira/Documents/database/cuhk_cufs_process"
+#casia_path = "/Users/tiago.pereira/Documents/database/CASIA-CleanedCroped_gray"
+#cuhk_cufs = "/Users/tiago.pereira/Documents/database/cuhk_cufs_process"
 #device = "/cpu:0"
-#l_rate = 0.01
+#l_rate = 0.1
 
-database_path = "/home/tiago-ttt/Documents/gitlab/database/CASIA-CleanedCroped_gray"
+casia_path = "/home/tiago-ttt/Documents/gitlab/database/CASIA-CleanedCroped_gray"
+cuhk_cufs = "/home/tiago-ttt/Documents/gitlab/database/cuhk_cufs_process"
 device = "/gpu:1"
 l_rate = 0.1
+
 iterations = 300000
+
+def map_labels(raw_labels):
+    """
+    Map the clients to 0 to 1
+    """
+    possible_labels = list(set(raw_labels))
+    labels = numpy.zeros(len(raw_labels), dtype="float")
+    raw_labels = numpy.array(raw_labels)
+
+    #print len(set(labels))
+    for i in range(len(possible_labels)):
+      l = float(possible_labels[i])
+      #print str(i) + " - " + str(len(numpy.where(labels==l)[0]))
+      labels[numpy.where(raw_labels==l)[0]] = float(i)
+    #print len(set(labels))
+    return labels
+
 
 
 def my_inception_2(inputs, scope='myInception', reuse=False, device="/cpu:0", n_classes=404):
@@ -341,12 +362,14 @@ def create_architecture(placeholder):
 
 # Loading data
 from bob.db.cuhk_cufs.query import Database
-database = Database(original_directory=database_path,
+database = Database(original_directory=cuhk_cufs,
                     original_extension=".hdf5",
                     arface_directory="", xm2vts_directory="")
 
 #normalizer = MeanOffsetHT(bob.io.base.load("means_visual.hdf5"), bob.io.base.load("means_sketch.hdf5"))
-normalizer = MeanOffset(bob.io.base.load("means.hdf5"))
+normalizer = MeanOffset(bob.io.base.load("means_casia.hdf5"))
+#normalizer = MeanOffset(bob.io.base.load("means.hdf5"))
+
 
 #train_data_shuffler = SiameseDiskHTFace(database=database, protocol="search_split1_p2s",
 #                                        batch_size=8,
@@ -354,11 +377,24 @@ normalizer = MeanOffset(bob.io.base.load("means.hdf5"))
 #                                        normalizer=MeanOffset(bob.io.base.load("means.hdf5")))
 
 
-train_data_shuffler = SiameseDiskHTFace(database=database, protocol="search_split1_p2s",
-                                        batch_size=8,
-                                        input_shape=[None, 224, 224, 1],
-                                        normalizer=normalizer,
-                                        data_augmentation=ImageAugmentation())
+#train_data_shuffler = SiameseDiskHTFace(database=database, protocol="search_split1_p2s",
+#                                        batch_size=8,
+#                                        input_shape=[None, 224, 224, 1],
+#                                        normalizer=normalizer,
+#                                        data_augmentation=ImageAugmentation())
+
+
+import bob.db.casia_webface
+db_casia = bob.db.casia_webface.Database()
+train_objects = sorted(db_casia.objects(groups="world"), key=lambda x: x.id)
+train_labels = map_labels([int(o.client_id) for o in train_objects])
+train_file_names = [o.make_path(directory=casia_path, extension=".png")
+    for o in train_objects]
+
+train_data_shuffler = SiameseDisk(train_file_names, train_labels,
+                           input_shape=[None, 224, 224, 1],
+                           batch_size=32,
+                           normalizer=normalizer)
 
 validation_data_shuffler = SiameseDiskHTFace(database=database, protocol="search_split1_p2s",
                                              batch_size=32,
@@ -366,11 +402,6 @@ validation_data_shuffler = SiameseDiskHTFace(database=database, protocol="search
                                              normalizer=normalizer,
                                              groups="dev",
                                              purposes=["enroll", "probe"])
-
-
-
-
-
 
 # Loss for the softmax
 loss = ContrastiveLoss()
