22464905e6a00ecfa966145abc999919abb59460
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2017-11-08 09:19:09 +0100

    Added siamese dataset for HTFAce

--- bob/bio/htface/architectures/Transfer.py
+++ bob/bio/htface/architectures/Transfer.py
@@ -6,13 +6,15 @@
 import tensorflow as tf
 slim = tf.contrib.slim
 
-def transfer_graph(inputs, get_embeddings=False, reuse=False, non_linearity_outputs=64, bottleneck_outputs=128):
+def transfer_graph(inputs, reuse=False, non_linearity_outputs=64, bottleneck_outputs=128):
 
     with tf.variable_scope('Transfer', reuse=reuse):
+        end_points={}
         prelogits = slim.fully_connected(inputs, non_linearity_outputs, activation_fn=tf.nn.relu, 
                                          weights_initializer=tf.truncated_normal_initializer(stddev=0.1), 
                                          weights_regularizer=slim.l2_regularizer(0.2),
                                          scope='NonLinearity')
+        end_points['NonLinearity1'] = prelogits
 
         #prelogits = slim.batch_norm(prelogits,
         #                           decay=0.95,
@@ -22,6 +24,7 @@ def transfer_graph(inputs, get_embeddings=False, reuse=False, non_linearity_outp
         prelogits = slim.fully_connected(prelogits, bottleneck_outputs, activation_fn=None, 
                                          weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
                                          scope='Bottleneck')
+        end_points['Bottleneck2'] = prelogits
 
-    return prelogits 
+    return prelogits, end_points
 
--- bob/bio/htface/test/test_dataset.py
+++ bob/bio/htface/test/test_dataset.py
@@ -4,30 +4,85 @@
 
 import tensorflow as tf
 from bob.bio.htface.dataset.siamese_htface import shuffle_data_and_labels_image_augmentation
-
+from bob.learn.tensorflow.utils import reproducible
 
 def test_siamease_dataset_cuhk_cufs():
+    """
+    from bob.db.cuhk_cufs.query import Database
+    database = Database(original_directory="/idiap/temp/tpereira/HTFace/CUHK-CUFS/RESNET_GRAY/INITIAL_CHECKPOINT/split1/preprocessed/",
+                        original_extension=".hdf5",
+                        arface_directory="", xm2vts_directory="")
 
-    #from bob.db.cuhk_cufs.query import Database
-    #database = Database(original_directory="/idiap/temp/tpereira/HTFace/CUHK-CUFS/RESNET_GRAY/INITIAL_CHECKPOINT/split1/preprocessed/",
-    #                    original_extension=".hdf5",
-    #                    arface_directory="", xm2vts_directory="")
+    dataset = shuffle_data_and_labels_image_augmentation(database, protocol="search_split1_p2s", data_shape=(160, 160, 1), data_type=tf.uint8,
+                                               batch_size=8, epochs=1, buffer_size=10**3,
+                                               gray_scale=False, 
+                                               output_shape=None,
+                                               random_flip=False,
+                                               random_brightness=False,
+                                               random_contrast=False,
+                                               random_saturation=False,
+                                               per_image_normalization=False, 
+                                               groups="world", purposes="train",
+                                               extension="hdf5")
+    offset = 0
+    session = tf.Session()
+    batch = session.run([dataset])
+    assert "left" in batch[0][0]
+    assert "right" in batch[0][0]
+    
+    import bob.io.base
+    import bob.io.image
+    #bob.io.base.save(batch[0][0]['right'][0,:,:,0].astype("uint8"), "0_right.png")
+    #bob.io.base.save(batch[0][0]['left'][0,:,:,0].astype("uint8"), "0_left.png")    
 
-    #dataset = shuffle_data_and_labels_image_augmentation(database, protocol="search_split1_p2s", data_shape=(160, 160, 1), data_type=tf.uint8,
-    #                                           batch_size=8, epochs=1, buffer_size=10**3,
-    #                                           gray_scale=False, 
-    #                                           output_shape=None,
-    #                                           random_flip=False,
-    #                                           random_brightness=False,
-    #                                           random_contrast=False,
-    #                                           random_saturation=False,
-    #                                           per_image_normalization=True, 
-    #                                           groups="world", purposes="train",
-    #                                           extension="hdf5")
-    #offset = 0
-    #session = tf.Session()
-    #batch = session.run([dataset])
-    #assert "left" in batch[0][0]
-    #assert "right" in batch[0][0]
+    #bob.io.base.save(batch[0][0]['right'][3,:,:,0].astype("uint8"), "1_right.png")
+    #bob.io.base.save(batch[0][0]['left'][3,:,:,0].astype("uint8"), "1_left.png")    
+
+    print batch[0][1]
+    """
+    
+    assert True
+
+
+def test_siamease_dataset_polathermal():
+    """
+    from bob.db.pola_thermal.query import Database
+    database = Database(original_directory="/idiap/temp/tpereira/HTFace/POLA_THERMAL/RESNET_GRAY/CASIA_WEBFACE/INITIAL_CHECKPOINT/split1/preprocessed/",
+                        original_extension=".hdf5")
+
+    dataset = shuffle_data_and_labels_image_augmentation(database, protocol="VIS-polarimetric-overall-split1", data_shape=(160, 160, 1), data_type=tf.uint8,
+                                               batch_size=8, epochs=1, buffer_size=10**3,
+                                               gray_scale=False, 
+                                               output_shape=None,
+                                               random_flip=False,
+                                               random_brightness=False,
+                                               random_contrast=False,
+                                               random_saturation=False,
+                                               per_image_normalization=True, 
+                                               groups="world", purposes="train",
+                                               extension="hdf5")
+    offset = 0
+    session = tf.Session()
+    labels = [] 
+    while True:
+        try:
+            batch = session.run([dataset])
+            assert "left" in batch[0][0]
+            assert "right" in batch[0][0]
+            labels += list(batch[0][1])
+        except:
+            break
+    import numpy
+    print numpy.mean(batch[0][0]['left'])
+    
+    #import bob.io.base
+    #import bob.io.image
+    #bob.io.base.save(batch[0][0]['right'][0,:,:,0].astype("uint16"), "0_right.png")
+    #bob.io.base.save(batch[0][0]['left'][0,:,:,0].astype("uint16"), "0_left.png")    
+
+    #bob.io.base.save(batch[0][0]['right'][1,:,:,0].astype("uint16"), "1_right.png")
+    #bob.io.base.save(batch[0][0]['left'][1,:,:,0].astype("uint16"), "1_left.png")    
+    #print batch[0][1]
+    """    
     assert True
 
