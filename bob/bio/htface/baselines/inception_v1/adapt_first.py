8d858ad1cb39054f3e2d3310b3b9b31423ee371b
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2018-02-23 19:48:44 +0100

    Added new baseline

--- bob/bio/htface/baselines/inception_v1/adapt_first.py
+++ bob/bio/htface/baselines/inception_v1/adapt_first.py
@@ -26,3 +26,22 @@ class SiameseAdaptFirstBatchNorm(Baseline):
         self.estimator         = pkg_resources.resource_filename("bob.bio.htface", "configs/tensorflow/siamese_transfer_learning/inception_resnet_v1_adapt_first_layer/estimator_nonshared_batch_norm.py")
         self.preprocessed_data = pkg_resources.resource_filename("bob.bio.htface", "configs/tensorflow/siamese_transfer_learning/inception_resnet_v2_databases/") # Same as v2
 
+
+class SiameseAdaptFirstBetasBatchNorm(Baseline):
+    """
+    This baseline has the following features:
+      - The prior uses batch norm in all layers
+      - Siamese net
+      - Adapt the first layer only
+    """
+
+    def __init__(self):
+        self.baseline_type     = "Siamese BN"
+        self.name              = "siamese_inceptionv1_first_layer_betas_nonshared_batch_norm"
+        self.extractor         = None
+        self.preprocessor      = pkg_resources.resource_filename("bob.bio.htface", "configs/experiments/inception_resnet_v2/preprocessor.py") # Same as v2
+        self.reuse_extractor   = False
+
+        # train cnn
+        self.estimator         = pkg_resources.resource_filename("bob.bio.htface", "configs/tensorflow/siamese_transfer_learning/inception_resnet_v1_adapt_first_layer/estimator_nonshared_betas_batch_norm.py")
+        self.preprocessed_data = pkg_resources.resource_filename("bob.bio.htface", "configs/tensorflow/siamese_transfer_learning/inception_resnet_v2_databases/") # Same as v2
