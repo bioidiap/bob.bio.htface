fcd39453973885c2efd77513ec20a7abaf9b6050
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2018-03-10 18:38:01 +0100

    triplet v1 baseline

--- bob/bio/htface/baselines/inception_v1/adapt_1_5.py
+++ bob/bio/htface/baselines/inception_v1/adapt_1_5.py
@@ -67,3 +67,19 @@ class TripletAdaptLayers1_5_BatchNorm(Baseline):
         self.preprocessed_data = pkg_resources.resource_filename("bob.bio.htface", "configs/tensorflow/siamese_transfer_learning/inception_resnet_v2_databases/")
 
 
+class TripletAdaptLayers1_5_BetasBatchNorm(Baseline):
+
+    def __init__(self):
+        super(TripletAdaptLayers1_5_BatchNorm, self).__init__()
+
+        self.baseline_type     = "Triplet BN adapt betas"
+        self.name              = "triplet_inceptionv1_layers_1_5_betas_nonshared_batch_norm"
+        self.extractor         = None
+        self.preprocessor      = pkg_resources.resource_filename("bob.bio.htface", "configs/experiments/inception_resnet_v2/preprocessor.py")
+        self.reuse_extractor   = False
+
+        # train cnn
+        self.estimator         = pkg_resources.resource_filename("bob.bio.htface", "configs/tensorflow/triplet_transfer_learning/inceptionv1_layers_1_5/estimator_nonshared_betas_batch_norm.py")
+        self.preprocessed_data = pkg_resources.resource_filename("bob.bio.htface", "configs/tensorflow/siamese_transfer_learning/inception_resnet_v2_databases/")
+
+
